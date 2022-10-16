d5ba805e909db24f1e6f1753f77fb36be230d0ae
Michael James Monday A <donsmartez@gmail.com>
2022-10-16 21:40:28 +0100

    code review commit

--- backend/src/api.py
+++ backend/src/api.py
@@ -10,10 +10,24 @@ from .auth.auth import AuthError, requires_auth
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
-
+'''
+@TODO uncomment the following line to initialize the datbase
+!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
+!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
+!! Running this funciton will add one
+'''
 db_drop_and_create_all()
 
 # ROUTES
+'''
+@TODO implement endpoint
+    GET /drinks
+        it should be a public endpoint
+        it should contain only the drink.short() data representation
+    returns status code 200 and json {"success": True, "drinks": drinks} 
+    where drinks is the list of drinks
+        or appropriate status code indicating reason for failure
+'''
 
 
 @app.route('/drinks', methods=['GET'])
@@ -26,6 +40,16 @@ def get_drinks():
     })
 
 
+'''
+@TODO implement endpoint
+    GET /drinks-detail
+        it should require the 'get:drinks-detail' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drinks}
+    where drinks is the list of drinks
+        or appropriate status code indicating reason for failure
+'''
+
 @app.route("/drinks-detail")
 @requires_auth("get:drinks-detail")
 def drinks_detail(jwt):
@@ -36,6 +60,18 @@ def drinks_detail(jwt):
         "success": True,
         "drinks": formattedDrink
     })
+
+'''
+@TODO implement endpoint
+    POST /drinks
+        it should create a new row in the drinks table
+        it should require the 'post:drinks' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drink} 
+    where drink an array containing only the newly created drink
+        or appropriate status code indicating reason for failure
+'''
+
 
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
@@ -53,6 +89,19 @@ def create_drink(jwt):
 
     return jsonify({"success": True, "drinks": [new_drink.long()]})
 
+
+'''
+@TODO implement endpoint
+    PATCH /drinks/<id>
+        where <id> is the existing model id
+        it should respond with a 404 error if <id> is not found
+        it should update the corresponding row for <id>
+        it should require the 'patch:drinks' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drink} 
+    where drink an array containing only the updated drink
+        or appropriate status code indicating reason for failure
+'''
 
 @app.route("/drinks/<int:drink_id>", methods=["PATCH"])
 @requires_auth("patch:drinks")
@@ -75,6 +124,18 @@ def update_drink(jwt, drink_id):
 
     return jsonify({"success": True, "drinks": [drink.long()]})
 
+
+'''
+@TODO implement endpoint
+    DELETE /drinks/<id>
+        where <id> is the existing model id
+        it should respond with a 404 error if <id> is not found
+        it should delete the corresponding row for <id>
+        it should require the 'delete:drinks' permission
+    returns status code 200 and json {"success": True, "delete": id} 
+    where id is the id of the deleted record
+        or appropriate status code indicating reason for failure
+'''
 
 @app.route("/drinks/<int:drink_id>", methods=["DELETE"])
 @requires_auth("delete:drinks")
@@ -101,9 +162,24 @@ def unprocessable(error):
     }), 422
 
 
-app.errorhandler(404)
+'''
+@TODO implement error handlers using the @app.errorhandler(error) decorator
+    each error handler should return (with approprate messages):
+             jsonify({
+                    "success": False,
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
+'''
 
 
+'''
+@TODO implement error handler for 404
+    error handler should conform to general task above
+'''
+
+
+@app.errorhandler(404)
 def resource_not_found(error):
     return jsonify({"success": False, "error": 404, "message": "not found"}), 404
 
